92bdcf24e21499d87298c809b8f28c6c1cf83374
kartikay1506 <kartikaysawhney150699@gmail.com>
2020-12-18 16:26:04 +0530

    added sending excel sheet to client in json format feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const excelToJson = require('convert-excel-to-json');
 const path = require('path');
 const app = express();
 
@@ -9,7 +10,20 @@ app.get('/', (req, resp) => {
 app.get('/get-master-data', (req, resp) => {
     resp.setHeader('Access-Control-Allow-Origin', '*');
     //resp.send("File Sent");
-    resp.sendFile(__dirname + "/src/PRICE LIST 17 NOV2020.xlsx");
+    //resp.sendFile(__dirname + "/src/PRICE LIST 17 NOV2020.xlsx");
+    var path = __dirname + "/src/PRICE LIST 17 NOV2020.xlsx";
+    const result = excelToJson({
+        sourceFile: path,
+        header: {
+            rows: 1
+        }
+    });
+    if(result) {
+        resp.send(result);
+    }
+    else {
+        resp.send("Error parsing excel sheet");
+    }
 });
 
 
