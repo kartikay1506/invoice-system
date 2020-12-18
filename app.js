17f9b2d3fd7fbe8ec937e98352a1464cf8dff200
kartikay1506 <kartikaysawhney150699@gmail.com>
2020-12-18 14:50:57 +0530

    Modified files

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, resp) => {
     resp.send("Welcome to the server!");
 });
+
+app.get('/get-master-data', (req, resp) => {
+    resp.setHeader('Access-Control-Allow-Origin', '*');
+    //resp.send("File Sent");
+    resp.sendFile(__dirname + "/src/PRICE LIST 17 NOV2020.xlsx");
+});
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server is running on port ${port}`));
